29d20324c3fac63c5bbf54fb2dbb67f1f95d75a4
SafaElmali <tsafaelmali@gmail.com>
2025-02-20 18:38:21 +0300

    fix: Update axios config

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,20 +1,16 @@
 import axios from "axios";
-import { cookies } from "next/headers";
-import { getCookie } from "cookies-next/server";
+import { getCookie } from "cookies-next/client";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Add request interceptor to add auth token
-axiosInstance.interceptors.request.use(async (config) => {
-  const token = await getCookie("token", { cookies });
+axiosInstance.interceptors.request.use((config) => {
+  const token = getCookie("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
-
-export { axiosInstance };
