ba230ad7aa1b11ff3d1c2991cdfedd771af16955
SafaElmali <tsafaelmali@gmail.com>
2025-02-20 18:31:50 +0300

    fix: Refactor post component & axios config

--- frontend/src/app/(blog)/_components/post/post-card.tsx
+++ frontend/src/app/(blog)/_components/post/post-card.tsx
@@ -24,7 +24,6 @@ const PostCard: FC<PostCardProps> = ({ post }) => {
             alt={post.title}
             fill
             className="object-cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
       </CardHeader>
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,18 +1,20 @@
-import axios from 'axios';
-import { getCookie } from 'cookies-next/client';
+import axios from "axios";
+import { cookies } from "next/headers";
+import { getCookie } from "cookies-next/server";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Add request interceptor to add auth token
-axiosInstance.interceptors.request.use((config) => {
-  const token = getCookie('token');
-  
+axiosInstance.interceptors.request.use(async (config) => {
+  const token = await getCookie("token", { cookies });
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
-export default axiosInstance; 
+export { axiosInstance };
