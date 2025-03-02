b05b9d915f95020eb97b1681850572e013f71934
SafaElmali <tsafaelmali@gmail.com>
2025-03-02 21:35:23 +0300

    feat: Add bubble menu & alignment

--- frontend/src/components/features/tiptap-editor/tiptap-editor.tsx
+++ frontend/src/components/features/tiptap-editor/tiptap-editor.tsx
@@ -4,10 +4,12 @@ import "./styles.css";
 import { cn } from "@/lib/utils";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { EditorToolbar } from "./_components/editor-toolbar";
+import { EditorBubbleMenu } from "./_components/bubble-menu";
 import { FC, useEffect } from "react";
 import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import StarterKit from "@tiptap/starter-kit";
+import TextAlign from "@tiptap/extension-text-align";
 
 type TiptapEditorProps = {
   value: string;
@@ -26,19 +28,29 @@ const TiptapEditor: FC<TiptapEditorProps> = ({
         heading: {
           levels: [1, 2],
         },
+        bulletList: {
+          keepMarks: true,
+          keepAttributes: false,
+        },
+        orderedList: {
+          keepMarks: true,
+          keepAttributes: false,
+        },
       }),
       Link.configure({
         openOnClick: false,
       }),
       Image,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+        alignments: ["left", "center", "right", "justify"],
+        defaultAlignment: "left",
+      }),
     ],
     content: value,
     editorProps: {
       attributes: {
-        class: cn(
-          "prose prose-stone dark:prose-invert prose-sm sm:prose-base lg:prose-lg w-full focus:outline-none min-h-[300px]",
-          className
-        ),
+        class: cn("tiptap", className),
       },
     },
     onUpdate: ({ editor }) => {
@@ -55,11 +67,9 @@ const TiptapEditor: FC<TiptapEditorProps> = ({
   return (
     <div className="w-full border rounded-md">
       <EditorToolbar editor={editor} />
-      <div
-        className="w-full min-h-[300px] p-4 overflow-y-auto cursor-text"
-        onClick={() => editor?.chain().focus().run()}
-      >
-        <EditorContent editor={editor} className="max-w-none" />
+      <div className="w-full min-h-[300px] p-4 overflow-y-auto cursor-text">
+        <EditorBubbleMenu editor={editor} />
+        <EditorContent editor={editor} />
       </div>
     </div>
   );
